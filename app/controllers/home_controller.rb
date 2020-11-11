cf349e95b03e2e128806ce84018d0ff74c91f908
zahidalik <zahidkhan545@icloud.com>
2020-11-11 18:33:20 +0300

    Adding all the static pages with google fonts and stylings

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,36 @@
 class HomeController < ApplicationController
+  before_action :current_date
+
   def index
+  end
+
+  def uans
+  end
+
+  def kemps
+  end
+
+  def wgs
+  end
+
+  def wbs
+  end
+
+  def a_level
+  end
+
+  def cttc
+  end
+
+  def journalism
+  end
+
+  def nursing
+  end
+
+  private
+
+  def current_date
+    @current_date = Time.now.strftime("%m %b %Y")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get 'about', to: 'home#about'
+  get 'uans', to: 'home#uans'
+  get 'kemps', to: 'home#kemps'
+  get 'wgs', to: 'home#wgs'
+  get 'wbs', to: 'home#wbs'
+  get 'cttc', to: 'home#cttc'
+  get 'journalism', to: 'home#journalism'
+  get 'nursing', to: 'home#nursing'
+  get 'a-level', to: 'home#a_level'
 end
